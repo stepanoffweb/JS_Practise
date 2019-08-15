257627c264bf4e4ca19d08e28ea67c0900ee3529
ivanstepanov <ivanstepanoff@yahoo.com>
2019-08-15 08:46:03 +0200

    some corrections done

--- glo_ozone/src/index.js
+++ glo_ozone/src/index.js
@@ -1,47 +1,82 @@
 'use strict';
 
 // checkbox
-const checkbox = document.getElementById('discount-checkbox');
+function toggleCheckbox() {
+    const checkbox = document.getElementById('discount-checkbox');
 
-checkbox.addEventListener('change', function() {
-    if(this.checked) {
-        this.nextElementSibling.classList.add('checked');
-    }
-    this.nextElementSibling.classList.remove('checked');
-});
+    checkbox.addEventListener('change', function () {
+        if (this.checked) {
+            this.nextElementSibling.classList.add('checked');
+        } else {
+            this.nextElementSibling.classList.remove('checked');
+        }
+    });
+}
+toggleCheckbox();
 // end checkbox
 // cart
-const btnCart = document.getElementById('cart');
-const modalCart = document.querySelector('.cart');
-const btnClose = document.querySelector('.cart-close');
+function toggleCart() {
+    const btnCart = document.getElementById('cart');
+    const modalCart = document.querySelector('.cart');
+    const btnClose = document.querySelector('.cart-close');
 
-btnCart.addEventListener('click', () => {
-    modalCart.style.cssText = 'display: flex';
-    document.body.style.overflow = 'hidden';
-});
-modalCart.addEventListener('click', () => {
-    modalCart.style.cssText = 'display: none';
-    document.body.style.overflow = '';
-});
+    btnCart.addEventListener('click', () => {
+        modalCart.style.cssText = 'display: flex';
+        document.body.style.overflow = 'hidden';
+    });
+    btnClose.addEventListener('click', () => {
+        modalCart.style.cssText = 'display: none';
+        document.body.style.overflow = '';
+    });
+}
+toggleCart();
 // end cart
 
 //  interaction with goods
-const cards = document.querySelectorAll('.goods .card'),
-    cartWrapper = document.querySelector('.cart-wrapper'),
-    cartEmpty = document.getElementById('cart-empty'),
-    countGoods = document.querySelector('.counter');
+function goodsChange() {
+    const cards = document.querySelectorAll('.goods .card'),
+        cartWrapper = document.querySelector('.cart-wrapper'),
+        cartEmpty = document.getElementById('cart-empty'),
+        countGoods = document.querySelector('.counter');
 
-cards.forEach((card) => {
-    const btn = card.querySelector('button');
-    btn.addEventListener('click', () => {
-        const cardClone = card.cloneNode(true);
-        cartWrapper.appendChild(cardClone);
-        cartEmpty.remove();
-        showQuantity();
+    // not optimized - hoisting would be better
+    cards.forEach((card) => {
+        const btn = card.querySelector('button');
+        btn.addEventListener('click', () => {
+            const cardClone = card.cloneNode(true),
+                removeBtn = cardClone.querySelector('.btn');
+            cartWrapper.appendChild(cardClone);
+            // cartEmpty.remove();
+            showQuantity();
+
+            removeBtn.textContent = 'Удалить из корзины';
+            removeBtn.addEventListener('click', () => {
+                cardClone.remove();
+                showQuantity();
+            });
+        });
     });
-});
 
-function showQuantity() {
-    const cartCards = cartWrapper.querySelectorAll('.card');
-     countGoods.textContent = cartCards.length;
+    function showQuantity() {
+        const cartCards = cartWrapper.querySelectorAll('.card'),
+            cardsPrice = cartWrapper.querySelectorAll('.card-price'),
+            cartTotal = document.querySelector('.cart-total span');
+        countGoods.textContent = cartCards.length;
+        let totalPrice = 0;
+        cardsPrice.forEach((cardPrice) => {
+            totalPrice += parseFloat(cardPrice.textContent);
+            // not optimized - recounting occures every goods adding
+        });
+        cartTotal.textContent = totalPrice;
+
+        if (cartCards.length) {
+            cartEmpty.remove();
+        } else {
+            cartWrapper.appendChild(cartEmpty);
+        }
+    }
 }
+goodsChange();
+// end interaction with goods
+
+// the filter "Акции"
