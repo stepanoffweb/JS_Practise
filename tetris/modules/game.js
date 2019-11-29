c1604d16cae15432d67e331b8cb5d0feeb9f9b21
ivanstepanov <ivanstepanoff@yahoo.com>
2019-11-29 16:03:59 +0100

    tetris get third rotation method with inner temp cell in for loops

--- tetris/modules/game.js
+++ tetris/modules/game.js
@@ -82,24 +82,34 @@ export default class Game {
         }
     }
     rotatePiece() {
+        this.rotateBlocks();
+        if(this.hasCollision()) {
+            this.rotateBlocks(false);
+        }
+    }
+
+    rotateBlocks(clockwise = true) {
         const blocks = this.activePiece.blocks;
         const length = blocks.length;
+        const x = Math.floor(length / 2);
+        const y = length - 1;
 
-        let temp = [];
-        for(let i = 0; i < length; i++) {
-            temp[i] = new Array(length).fill(0);
-        }
-        // строки делаем столбцами...:)
-        for (let y = 0; y < length; y++) {
-            for (let x = 0; x < length; x++) {
-                temp[x][y] = blocks[length - 1 - y][x];
+        for (let i = 0; i < blocks.length; i++) {
+            for (let j = i; j < y - i; j++) {
+                const temp = blocks[i][j];
+                if(clockwise) {
+                    blocks[i][j] = blocks[y- j][i];
+                    blocks[y - j][i] = blocks[y- i][y - j];
+                    blocks[y -i][y - j] = blocks[j][y - i];
+                    blocks[j][y - i] = temp;
+                } else {
+                    blocks[i][j] = blocks[j][y - i];
+                    blocks[j][y - i] = blocks[y- i][y - j];
+                    blocks[y -i][y - j] = blocks[y - j][i];
+                    blocks[y - j][i] = temp;
+                }
             }
         }
-         this.activePiece.blocks = temp;
-
-         if(this.hasCollision()) {
-              this.activePiece.blocks = blocks;
-         }
     }
 
 
