5ab803014b78f12c4dfbdc1df49b9ae48264c024
ivanstepanov <ivanstepanoff@yahoo.com>
2019-12-27 07:41:41 +0200

    1_jquery 2_modal_popups 3_tabs 4_sliders 5_menu_adaptive 6_calculator are added

---
+++ 2_modal_popups/final/js/main.js
@@ -0,0 +1,38 @@
+/* кое-что о всплывающих (модальных) окнах (не хватает fadeIn| Out по opacity)*/
+
+// Самый простой вариант
+// $(document).ready(function() {
+//     var modal = $('.popup'),
+//         overlay = $('.overlay'),
+//         link = $('button[data-popup="true"]'),
+//         close = $('.close-btn');
+//     link.on('click', function() {
+//         overlay.show();
+//         modal.show();
+//     });
+//     close.click(function() {
+//         overlay.hide();
+//         modal.hide();
+//     });
+// });
+
+// вылет окна из-за края экрана + много кнопок - одно окно (обработчики на элементы по атрибутам 'data-') + вставка текста из значения атрибута кнопки
+$(document).ready(function() {
+    var modal = $('.popup'),
+        overlay = $('.overlay'),
+        link = $('button[data-popup="true"]'),
+        close = $('.close-btn'),
+        fruitName = $('.fruit-name');
+
+    close.click(function() {
+        modal.toggleClass('popup_active');
+        overlay.hide();
+    });
+    link.on('click', function() {
+        fruitName.text($(this).attr('data-fruit'));
+        overlay.show();
+        modal.toggleClass('popup_active');
+    });
+});
+
+// функции jQuery hide()| show() изменяют display: none| block, что не позволяет анимировать появление
