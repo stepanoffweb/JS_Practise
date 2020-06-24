9333f4cbea5fece10b22bc28d3b876c1a30a32e9
ivanstepanov <ivanstepanoff@yahoo.com>
2020-06-24 06:55:19 +0300

    added options destroy(), setContent, addButtons to the plugin & dinamic content on user page 10_js_css_plugins

--- s_modal_popups/10_js_css_plugins/plugins/modal.js
+++ s_modal_popups/10_js_css_plugins/plugins/modal.js
@@ -1,17 +1,37 @@
+Element.prototype.insertAfter = function (elem) {
+  console.log('this ', this);
+  elem.parentNode.insertBefore(this, elem.nextSibling)
+}
+
+const _createFooter = function(btns) {
+  const wrap = document.createElement('div')
+  wrap.classList.add('modal-footer')
+  btns.forEach(btn => {
+    const $btn = document.createElement('button')
+    $btn.classList.add(`btn-${btn.type}`)
+    $btn.innerText = btn.text
+    $btn.addEventListener('click', () => {
+      alert(btn.handler)
+    })
+    wrap.appendChild($btn)
+  })
+  return wrap
+}
+
 const _createModal = function(options) {
   const data = options || {}
   const modal = document.createElement('div')
   modal.classList.add('smodal')
+  const DEFAULT_WIDTH = '600px'
   modal.insertAdjacentHTML('afterbegin', `
-    <div class="modal-overlay">
-        <div class="modal-window">
+    <div class="modal-overlay" data-close="true" >
+        <div class="modal-window"  style="width: ${data.width || DEFAULT_WIDTH}">
             <div class="modal-header">
                 <span class="modal-title">${data.title || 'Modal title'}</span>
-                ${data.closable ? '<span class="modal-close">&times;</span>' : ''}
+                ${data.closable ? '<span class="modal-close" data-close="true">&times;</span>' : ''}
             </div>
-            <div class="modal-text">
-                <p>Lorem ipsum dolor sit amet.</p>
-                <p>Lorem ipsum dolor sit amet.</p>
+            <div class="modal-text" data-content="content">
+                ${data.content || '<p>The default content</p>'}
             </div>
             <div class="modal-footer">
                 <button>OK</button>
@@ -19,6 +39,8 @@ const _createModal = function(options) {
             </div>
         </div>
     </div>`)
+  const footer = _createFooter(data.btns)
+  footer.insertAfter(modal.querySelector('[data-content]'))
   document.body.appendChild(modal)
   return modal
 }
@@ -27,8 +49,11 @@ $.modal = function (options) {
   const $modal = _createModal(options)
   const ANIMATION_SPEED = 2000
   let closing = false
-  return {
+  let isDestroyed = false
+
+  const modal = {
     open() {
+      if (isDestroyed) return alert('Modal is destroyed!')
       !closing && $modal.classList.add('active')
     },
     close() {
@@ -39,8 +64,24 @@ $.modal = function (options) {
         $modal.classList.remove('closing')
         closing = false
       }, ANIMATION_SPEED)
-    },
-    destroy() {}
+    }
   }
+  const listener = e => {
+    if (e.target.dataset.close) {
+      modal.close()
+    }
+  }
+
+  document.addEventListener('click', listener)
+
+  return Object.assign(modal, {
+    destroy() {
+      $modal.parentNode.removeChild($modal)
+      document.removeEventListener('click', listener)
+      isDestroyed = true
+    },
+    setContent(html) {
+      document.querySelector('[data-content]').innerHTML = html
+    }
+  })
 }
-// объект методов с доступом  по замыканию
--- s_modal_popups/10_js_css_plugins/script.js
+++ s_modal_popups/10_js_css_plugins/script.js
@@ -1,10 +1,33 @@
 const myModal = $.modal({
   title: 'Hello!',
-  closable: false,
+  closable: true,
+  content: `<h1>Hello! I'm a new content!</h1>`,
+  // width: '900px',
+  btns: [
+  {text: 'Показать цену', type: 'primary', handler: 'Primary is clicked'},
+  {text: 'Удалить', type: 'danger', handler: 'Danger is clicked'}
+  ],
   }) // !создается! инстанс
 
-document.addEventListener('click', (e) => {
-  if (e.target === document.querySelector('.modal-close') || e.target === document.querySelector('.modal-overlay')) {
-    myModal.close()
-  }
+
+const cards = [
+{id:1, text: "Salad", img: 'https://e2.edimdoma.ru/data/ingredients/0000/9296/9296-ed4_wide.jpg?1524483388'},
+{id:2, text: "Tomatoes", img: 'https://e1.edimdoma.ru/data/ingredients/0000/2168/2168-ed4_small.jpg?1524484144'},
+{id:3, text: "Lemons", img: 'https://e2.edimdoma.ru/data/ingredients/0000/1088/1088-ed4_small.jpg?1487686969'}
+]
+
+cards.forEach(card => {
+  const wrapper = document.createElement('div')
+  wrapper.classList.add('col')
+  wrapper.insertAdjacentHTML('afterbegin', `
+    <div class="card" >
+  <img src="${card.img} class="card-img-top" style="height: 200px;" alt=${card.text}>
+  <div class="card-body">
+    <h5 class="card-title">${card.text}</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary" onClick="(myModal.open())()" >Show price</a>
+    <a href="#" class="btn btn-danger" onClick="() => myModal.close()" >Delete</a>
+   </div>
+   </div> `)
+  document.querySelector('.cards').appendChild(wrapper)
 })
