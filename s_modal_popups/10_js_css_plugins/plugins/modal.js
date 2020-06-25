feda54c0a29caa46c26a451cc2c156e5eafba364
ivanstepanov <ivanstepanoff@yahoo.com>
2020-06-25 07:07:50 +0300

    added options getNode, getListener & create dinamic html with listeners that interfere with plagin and 2 modals in  10_js_css_plugins

--- s_modal_popups/10_js_css_plugins/plugins/modal.js
+++ s_modal_popups/10_js_css_plugins/plugins/modal.js
@@ -1,24 +1,23 @@
 Element.prototype.insertAfter = function (elem) {
-  console.log('this ', this);
+  // console.log('this ', this);
   elem.parentNode.insertBefore(this, elem.nextSibling)
 }
+const noop = () => {}
 
-const _createFooter = function(btns) {
+const _createFooter = function (btns) {
   const wrap = document.createElement('div')
   wrap.classList.add('modal-footer')
   btns.forEach(btn => {
     const $btn = document.createElement('button')
-    $btn.classList.add(`btn-${btn.type}`)
+    $btn.classList.add('btn', `btn-${btn.type}`)
     $btn.innerText = btn.text
-    $btn.addEventListener('click', () => {
-      alert(btn.handler)
-    })
+    $btn.onclick = btn.handler || noop
     wrap.appendChild($btn)
   })
   return wrap
 }
 
-const _createModal = function(options) {
+const _createModal = function (options) {
   const data = options || {}
   const modal = document.createElement('div')
   modal.classList.add('smodal')
@@ -27,15 +26,11 @@ const _createModal = function(options) {
     <div class="modal-overlay" data-close="true" >
         <div class="modal-window"  style="width: ${data.width || DEFAULT_WIDTH}">
             <div class="modal-header">
-                <span class="modal-title">${data.title || 'Modal title'}</span>
+                <span class="modal-title" data-header="header">${data.title || 'Modal title'}</span>
                 ${data.closable ? '<span class="modal-close" data-close="true">&times;</span>' : ''}
             </div>
             <div class="modal-text" data-content="content">
                 ${data.content || '<p>The default content</p>'}
-            </div>
-            <div class="modal-footer">
-                <button>OK</button>
-                <button>Fuck</button>
             </div>
         </div>
     </div>`)
@@ -47,41 +42,51 @@ const _createModal = function(options) {
 
 $.modal = function (options) {
   const $modal = _createModal(options)
-  const ANIMATION_SPEED = 2000
+  const ANIMATION_SPEED = 200
   let closing = false
   let isDestroyed = false
 
   const modal = {
-    open() {
+    open () {
       if (isDestroyed) return alert('Modal is destroyed!')
       !closing && $modal.classList.add('active')
     },
-    close() {
+    close () {
       closing = true
-      $modal.classList.remove('active')
       $modal.classList.add('closing')
+      $modal.classList.remove('active')
       setInterval(() => {
         $modal.classList.remove('closing')
         closing = false
       }, ANIMATION_SPEED)
     }
   }
-  const listener = e => {
-    if (e.target.dataset.close) {
-      modal.close()
-    }
-  }
+  // const listener = e => {
+  //   if (e.target.dataset.close) {
+  //     modal.close()
+  //   }
+  // }
 
-  document.addEventListener('click', listener)
-
+  // document.addEventListener('click', listener)
+  let node, listener
   return Object.assign(modal, {
-    destroy() {
-      $modal.parentNode.removeChild($modal)
+    destroy () {
+      // $modal.parentNode.removeChild($modal)
+      node.parentNode.removeChild(node)
       document.removeEventListener('click', listener)
-      isDestroyed = true
+      // isDestroyed = true
     },
-    setContent(html) {
+    setContent (html) {
       document.querySelector('[data-content]').innerHTML = html
+    },
+    setHeader (html) {
+      document.querySelector('[data-header]').innerHTML = html
+    },
+    getNode (target) {
+      node = target
+    },
+    getListener(listener) {
+      listener = listener
     }
   })
 }
